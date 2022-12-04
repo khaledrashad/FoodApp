d6b40fffb9aa04bc434bc88d6394237ae827c3fc
Khaled rashad <khaledamr400@gmail.com>
2022-12-04 15:23:56 +0200

    Modal

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import Header from "./components/header/Header/Header";
 import Meals from "./components/meals/Meals";
+import Cart from "./components/header/cart/Cart";
 
 
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <Meals />
     </Fragment>
